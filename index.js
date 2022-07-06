2cb7f7ffde8579198bfebbf35c58f18eb8293a2d
Aleksei Zakharov <alexey.v.zaharov@gmail.com>
2022-07-05 22:39:40 -0500

    hack hack

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('crypto').randomBytes
+var random = require('expo-random')
+
+module.exports = (strength) => {
+    return Buffer.from(random.getRandomBytes(strength));
+  };
